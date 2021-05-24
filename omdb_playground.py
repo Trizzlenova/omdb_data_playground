00ca3b497ccda2ad1d06381bd3cd02f45508a346
Trizzlenova <tdswayzee@gmail.com>
2021-05-23 21:23:20 -0700

    created line graph to vis season avg

--- omdb_playground.py
+++ omdb_playground.py
@@ -12,6 +12,34 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+# UPDATING SCRIPT WITH OMDB LIBRARY
+import omdb
+
+res = omdb.request(t='south park', tomatoes=True)
+
+def urlify_string(string):
+    return string.replace(' ', '+')
+
+def get_show_details(URL, KEY, TITLE):
+    params = {
+        'apikey': KEY,
+        't': urlify_string(TITLE),
+        'r': 'json'
+    }
+    response = requests.get(URL, params=params)
+    details = json.loads(response.text)
+    if details['Type'] == 'series':
+        title = details['Title']
+        return {'title': title,
+                'year': details['Year'],
+                'plot': details['Plot'],
+                'rating': details['imdbRating'],
+                'votes': details['imdbVotes'],
+                'url': f"https://www.imdb.com/title/{details['imdbID']}/",
+                'show_df': get_and_save_season(URL, KEY, title)}
+    else:
+        return 'meh...nottashobruh'
+     
 
 
 key=KEY
@@ -19,10 +47,12 @@ url = 'http://www.omdbapi.com/'
 show='south park'
 season=1
 
-def urlify_string(string):
-    return string.replace(' ', '+')
+def get_top_episode_details(DATA):
+    return DATA.iloc[DATA['imdbRating'].idxmax()]
+
 
 def get_and_save_season(URL, KEY, SHOW, SEASON=1, ALL_EPISODES=[]):
+    omdb.set_default('apikey', KEY)
     params = {
         'apikey': KEY,
         't': urlify_string(SHOW),
@@ -36,7 +66,6 @@ def get_and_save_season(URL, KEY, SHOW, SEASON=1, ALL_EPISODES=[]):
         ALL_EPISODES = []
         
     if data['Response']:
-        print(SEASON)
         ALL_EPISODES.append(data)
             
         next_season = int(data['Season']) + 1
@@ -88,21 +117,40 @@ def convert_column_to_numeric(DATA, COLUMN_LIST):
 
 # TODO: see if clickable links can be added (click a cell to go to imdb ep page)
 df = get_and_save_season(URL, KEY, SHOW)
-ratings = pd.pivot_table(df, 'imdbRating', 'Season', 'numberInSeason')
+
+def pivot_table_for_plot_setup(DATA):
+    ratings_df = pd.pivot_table(DATA, 'imdbRating', 'Season', 'numberInSeason')
+    ratings_df['average_rating'] = ratings_df.mean(numeric_only=True, axis=True)
+    ratings_df['Season'] = ratings_df.index
+    return ratings_df
+    
+    
+def create_line_plot(DATA):
+    lines = DATA.plot.line(x='Season', y='average')
+    return lines
 
 
 #
 plt.figure(figsize=(24,20))
-ax = sns.heatmap(ratings, annot=True, linewidths=1, cmap=mpl.cm.get_cmap('RdYlGn'))
+ax = sns.heatmap(ratings, 
+                 annot=True, 
+                 linewidths=1, 
+                 cmap=mpl.cm.get_cmap('RdYlGn'), 
+                 cbar=False)
 ax.xaxis.tick_top()
 ax.xaxis.set_label_position('top')
 ax.tick_params(length=0)
 ax.tick_params(axis='both', which='major', labelsize=24)
 ax.tick_params(axis='both', which='minor', labelsize=24)
+font_size = 24
 plt.rc('font', size=font_size)
 plt.rc('axes', titlesize=font_size)
-label_style = {'fontname':'Helvetica','fontsize':'24'}
-plt.xlabel('Episode Number', **label_style)
+label_style = {'fontname':'Helvetica','fontsize':font_size}
+plt.xlabel('Episode', **label_style)
 plt.ylabel('Season', **label_style)
 plt.yticks(rotation=0)
+ax.grid(False)
 plt.show()
+
+
+
